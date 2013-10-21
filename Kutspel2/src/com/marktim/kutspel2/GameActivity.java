8e00a7e4044ecc52f6a5971a5a979aa1944eb128
markbuikema <mark_buikema@hotmail.com>
2013-10-21 20:33:10 +0200

    created instance of renderer setrenderer on GLGameView

--- Kutspel2/src/com/marktim/kutspel2/GameActivity.java
+++ Kutspel2/src/com/marktim/kutspel2/GameActivity.java
@@ -3,16 +3,20 @@ package com.marktim.kutspel2;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.marktim.kutspel2.opengl.GLGameRenderer;
 import com.marktim.kutspel2.opengl.GLGameView;
 
 public class GameActivity extends Activity {
 
 	private GLGameView view;
+	private GLGameRenderer renderer;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         view = new GLGameView(this);
+        renderer = new GLGameRenderer(this);
+        view.setRenderer(renderer);
         
         view.setKeepScreenOn(BuildConfig.DEBUG);
         setContentView(view);
--- Kutspel2/src/com/marktim/kutspel2/opengl/GLGameRenderer.java
+++ Kutspel2/src/com/marktim/kutspel2/opengl/GLGameRenderer.java
@@ -3,9 +3,16 @@ package com.marktim.kutspel2.opengl;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import android.content.Context;
 import android.opengl.GLSurfaceView.Renderer;
 
 public class GLGameRenderer implements Renderer{
+
+	private Context context;
+	
+	public GLGameRenderer(Context context) {
+		this.context = context;
+	}
 
 	@Override
 	public void onDrawFrame(GL10 arg0) {
